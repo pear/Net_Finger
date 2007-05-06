26159afcb0849354c386f65b165cd8b2fa0d861d
Sebastian Nohn <nohn@php.net>
2007-05-06 14:20:00 +0000

    * add package2.xml, fix coding style
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_Finger/trunk@235053 c90b9560-bf6c-de11-be94-00142212c4b1

--- Finger.php
+++ Finger.php
@@ -1,23 +1,29 @@
 <?php
-//
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Sebastian Nohn <sebastian@nohn.net>                         |
-// +----------------------------------------------------------------------+
-//
-// $Id$
-//
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * PEAR::Net_Finger
+ *
+ * This class acts as interface to finger servers
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  Net
+ * @package   Net_Finger
+ * @author    Sebastian Nohn <sebastian@nohn.net>
+ * @copyright 2003-2007 Sebastian Nohn <sebastian@nohn.net>
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   CVS: $id$
+ * @link      http://pear.php.net/package/Net_Finger
+ * @since     File available since Release 1.0.0
+ */
 
 require_once 'PEAR.php';
 require_once 'Net/Socket.php';
@@ -27,9 +33,12 @@ require_once 'Net/Socket.php';
  *
  * Provides functions useful for Finger-Queries.
  *
- * @version $Revision$
- * @author Sebastian Nohn <sebastian@nohn.net>
- * @package Net
+ * @category Net
+ * @package  Net_Finger
+ * @author   Sebastian Nohn <sebastian@nohn.net>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version  Release: @package_version@
+ * @link     http://pear.php.net/package/Net_Finger
  */
 class Net_Finger
 {
@@ -37,18 +46,19 @@ class Net_Finger
     /**
      * Implements Net_Finger::query() function using PEAR's socket functions
      *
-     * @param 	string	$server The finger-server to query
-     * @param 	string  $query	The finger database object to lookup
-     * @return 	mixed  			The data returned from the finger-server as string
-     *                          or a PEAR_Error ( see Net_Socket for error codes)
+     * @param string $server The finger-server to query
+     * @param string $query  The finger database object to lookup
+     *
+     * @return mixed  		  The data returned from the finger-server as string
+     *                        or a PEAR_Error ( see Net_Socket for error codes)
      */   
     function query($server, $query)
     {
         $socket = new Net_Socket;
-        if( PEAR::isError( $sockerror = $socket->connect($server, 79))) {
-            $data = new PEAR_Error( "Error connecting to $server ( Net_Socket says: ".
-                                    $sockerror->getMessage().")", $sockerror->getCode());
-        } else { 
+        if (PEAR::isError($sockerror = $socket->connect($server, 79))) {
+            $data = new PEAR_Error("Error connecting to $server (Net_Socket says: ".
+                        $sockerror->getMessage().")", $sockerror->getCode());
+        } else {
             $query .= "\n"; 
             $socket->write($query); 
             $data = $socket->read(16384); 
